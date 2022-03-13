ca02cd1bfb45b560251584cc18767a42b193eb15
Jakub Labryszewski <stiwsquer99@gmail.com>
2022-03-13 14:49:31 +0100

    set project up

---
+++ src/pages/HotelsPage.tsx/HotelsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const HotelsPage = (props: Props) => {
+  return <h1 className="text-3xl font-bold underline">Hotels Page</h1>;
+};
+
+export default HotelsPage;
