20dc5e6f5ba0319ec552aebf4e44110daaf6b2bd
Jakub Labryszewski <stiwsquer99@gmail.com>
2022-03-19 17:29:35 +0100

    feat: populate room component

--- src/components/Hotel/Hotel.tsx
+++ src/components/Hotel/Hotel.tsx
@@ -1,4 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 import Rating from '@mui/material/Rating';
+import Collapse from '@mui/material/Collapse';
 import { HotelDTO } from '../../services/GetHotels/GetHotels.types';
 import useGetRooms from '../../services/GetRooms/GetRooms.service';
 import Slider from '../Slider/Slider';
@@ -9,17 +13,20 @@ type Props = {
 };
 const Hotel = ({ hotel }: Props) => {
   const { roomsData, isFetching, error } = useGetRooms(hotel.id);
+  const [expand, setExpand] = useState(true);
 
   if (error) return null;
-  if (isFetching) return <h1>Fetching room...</h1>;
+  if (isFetching) return <h1>Fetching rooms...</h1>;
 
   return (
-    <article className="w-full my-6 mx-2  flex flex-col shadow-2xl">
-      <section className="flex flex-row w-full flex-wrap-reverse justify-center">
+    <article className="w-full my-6 flex flex-col shadow-2xl rounded-lg">
+      <section className="flex flex-row w-full flex-wrap-reverse justify-center relative sm:pb-0 pb-5">
         <Slider images={hotel.images} />
         <div className="flex flex-row justify-between flex-1 basis-80 ">
           <div className="m-4">
-            <h1 className="font-bold text-xl mb-3">{hotel.name}</h1>
+            <h1 className="font-bold text-xl mb-3 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+              {hotel.name}
+            </h1>
             <h3 className=" text-sm mb-1">{hotel.address1}</h3>
             <h3 className=" text-sm">{hotel.address2}</h3>
           </div>
@@ -30,10 +37,28 @@ const Hotel = ({ hotel }: Props) => {
             readOnly
           />
         </div>
+        <button
+          className="absolute bottom-0 right-4 text-violet-500"
+          type="button"
+          onClick={() => setExpand((prev) => !prev)}
+        >
+          <FontAwesomeIcon
+            className={
+              expand
+                ? 'rotate-180 transition-transform'
+                : 'transition-transform'
+            }
+            icon={faAngleDown}
+            size="2x"
+          />
+        </button>
       </section>
-      {roomsData?.rooms.map((room) => (
-        <Room key={room.id} room={room} />
-      ))}
+
+      <Collapse in={expand} timeout="auto" unmountOnExit>
+        {roomsData?.rooms.map((room) => (
+          <Room key={room.id} room={room} />
+        ))}
+      </Collapse>
     </article>
   );
 };
--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -4,9 +4,23 @@ type Props = {
   room: RoomInterface;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Room({ room }: Props) {
-  return <div>Room</div>;
+  return (
+    <section className="m-4 flex border-t border-slate-400 pt-4">
+      <div className="basis-60">
+        <h2 className="font-bold text-md mb-2">{room.name}</h2>
+        <div className="flex">
+          <h4 className="mr-1">Adults:</h4>
+          <div>{room.occupancy.maxAdults}</div>
+        </div>
+        <div className="flex">
+          <h4 className="mr-1">Children:</h4>
+          <div>{room.occupancy.maxChildren}</div>
+        </div>
+      </div>
+      <div className="basis-60 flex-1 text-sm ">{room.longDescription}</div>
+    </section>
+  );
 }
 
 export default Room;
